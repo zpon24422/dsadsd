b66786e4ebe9b40aea6be1ee114a80ea9ad55c2f
zpon24422 <121872140+zpon24422@users.noreply.github.com>
2024-02-20 16:40:52 +0300

    Add files via upload

---
+++ autoshopDjimbo3.4/tgbot/utils/misc/bot_filters.py
@@ -0,0 +1,57 @@
+# - *- coding: utf- 8 - *-
+from aiogram import types
+from aiogram.dispatcher.filters import BoundFilter
+
+from tgbot.data.config import get_admins
+from tgbot.services.api_sqlite import get_settingsx
+
+
+# Проверка на диалог в ЛС бота
+class IsPrivate(BoundFilter):
+    async def check(self, message):
+        if "id" in message:
+            return message.message.chat.type == types.ChatType.PRIVATE
+        else:
+            return message.chat.type == types.ChatType.PRIVATE
+
+
+# Проверка на админа
+class IsAdmin(BoundFilter):
+    async def check(self, message: types.Message):
+        if message.from_user.id in get_admins():
+            return True
+        else:
+            return False
+
+
+# Проверка на технические работы
+class IsWork(BoundFilter):
+    async def check(self, message: types.Message):
+        get_settings = get_settingsx()
+
+        if get_settings['status_work'] == "False" or message.from_user.id in get_admins():
+            return False
+        else:
+            return True
+
+
+# Проверка на возможность пополнения
+class IsRefill(BoundFilter):
+    async def check(self, message: types.Message):
+        get_settings = get_settingsx()
+
+        if get_settings['status_refill'] == "True" or message.from_user.id in get_admins():
+            return False
+        else:
+            return True
+
+
+# Проверка на возможность покупки товара
+class IsBuy(BoundFilter):
+    async def check(self, message: types.Message):
+        get_settings = get_settingsx()
+
+        if get_settings['status_buy'] == "True" or message.from_user.id in get_admins():
+            return False
+        else:
+            return True
